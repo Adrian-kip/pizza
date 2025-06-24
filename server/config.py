1c75e42338136f3fd1023f0d2b9bf9446d182892
Adrian Kiptoo <cherukip.adrian@gmail.com>
2025-06-24 10:29:22 +0300

    Install the dependancies

--- server/config.py
+++ server/config.py
@@ -1,8 +1,12 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    print("Warning: python-dotenv not installed. Using default configuration.")
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///pizza_restaurant.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///pizza_restaurant.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
